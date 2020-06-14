afbd35fba2152c67053402eb310d4ec3b2b06bd9
clyzby <jose.conchello@gmail.com>
2020-06-14 12:09:48 -0400

    master - add unit test to NoteCardComponent

--- tests/unit/NoteCardComponent.spec.ts
+++ tests/unit/NoteCardComponent.spec.ts
@@ -3,18 +3,22 @@ import Vue from "vue";
 import chai, { expect } from "chai";
 import sinon from "sinon";
 import spies from "chai-spies";
-chai.use(spies);
-// import sinonChai from "sinon-chai";
 import { shallowMount, Wrapper } from "@vue/test-utils";
+import VueRouter from "vue-router";
+
+chai.use(spies);
 
 // ==== App Imports =======================================================
 import NoteCardComponent from "@/components/NoteCardComponent.vue";
-// import { NoteObject } from "@/types";
+
+const router = new VueRouter();
 
 describe("NoteCardComponent.vue", () => {
   let wrapper: Wrapper<Vue>;
 
-  function mountComponentWithProperties(componentProps: Record<string, any>): void {
+  function mountComponentWithProperties(
+    componentProps: Record<string, any>,
+  ): void {
     wrapper = shallowMount(NoteCardComponent, {
       propsData: componentProps,
     });
@@ -92,31 +96,69 @@ describe("NoteCardComponent.vue", () => {
     expect((wrapper.vm as any).isExistingNote).to.be.true;
   });
 
-  it.only("should call 'handleSave' on save button click", async () => {
+  it("should call 'handleSave' on save button click", async () => {
     // GIVEN
     const note = {
       title: "THIS IS A TITLE",
       body: "THIS IS MY BODY - isn't it rad",
       id: 2010,
-    }
+    };
 
     mountComponentWithProperties(note);
-    const spy = chai.spy((wrapper.vm as any).handleSave)
-    const vm = wrapper.vm as any
-    sinon.spy(vm, "handleSave")
-    wrapper.setData({
-      note: note,
-      valid: true,
-      saving: false,
-    });
+    const spy = chai.spy((wrapper.vm as any).handleSave);
+    const vm = wrapper.vm as any;
+    sinon.spy(vm, "handleSave");
 
-
-    // WHEN 
+    // WHEN
     const saveButtonEl = wrapper.find("#save-btn");
     saveButtonEl.vm.$emit("click");
     await Vue.nextTick();
 
     // THEN
     expect(spy).to.have.been.called;
+  });
+
+  it("should call 'updateNote' on save button click", async () => {
+    // GIVEN
+    const note = {
+      title: "THIS IS A TITLE",
+      body: "THIS IS MY BODY - isn't it rad",
+      id: 2010,
+    };
+
+    mountComponentWithProperties(note);
+    const spy = chai.spy((wrapper.vm as any).updateNote);
+    const vm = wrapper.vm as any;
+    sinon.spy(vm, "updateNote");
+
+    // WHEN
+    const saveButtonEl = wrapper.find("#save-btn");
+    saveButtonEl.vm.$emit("click");
+    await Vue.nextTick();
+
+    // THEN
+    expect(spy).to.have.been.called;
+  });
+
+  it("should call 'saveNote' on save button click", async () => {
+    // GIVEN
+    const note = {
+      title: "THIS IS A TITLE",
+      body: "THIS IS MY BODY - isn't it rad",
+      id: -1,
+    };
+
+    mountComponentWithProperties(note);
+    const spy = chai.spy((wrapper.vm as any).saveNote);
+    const vm = wrapper.vm as any;
+    sinon.spy(vm, "saveNote");
+
+    // WHEN
+    const saveButtonEl = wrapper.find("#save-btn");
+    saveButtonEl.vm.$emit("click");
+    await Vue.nextTick();
+
+    // THEN
+    expect(spy).to.have.been.to.have.been.called;
   });
 });
